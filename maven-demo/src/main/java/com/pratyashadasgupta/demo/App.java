67d5a26b71bd27e5edf0d5995e91293de7095e61
PRASUN DASGUPTA <dasguptapratyasha@gmail.com>
2020-02-06 21:36:45 +0530

    first commit

---
+++ maven-demo/src/main/java/com/pratyashadasgupta/demo/App.java
@@ -0,0 +1,13 @@
+package com.pratyashadasgupta.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
